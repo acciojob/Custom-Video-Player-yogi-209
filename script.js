a8e32f88f52dd67fab4dae23d7cf09d5e45c9b69
Boyanapalli Yogendra sai <boyanapalliyogendrasai@gmail.com>
2026-09-25 10:36:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function handleRangeUpdate() {
 function handleprogress()
 {
 	const percent=(video.currentTime/video.duration)*100;
-	progressBar.style.flexbasis='${percent}%'
+	progressBar.style.flexBasis='${percent}%'
 }
 function scrub(e){
 	const scrubTime=(e.offsetX/progress.clientWidth)*video.duration;
